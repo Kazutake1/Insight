57838d5ad459acc4038bea8fe82c5ce350a04554
Kazutake1 <i8ayxda1tonezzo@yahoo.co.jp>
2026-09-21 22:30:53 +0900

    Load dashboard KPI inset and confirmed fiscal-year deletion controls

--- insight_ops_kpifix_v1.js
+++ insight_ops_kpifix_v1.js
@@ -207,3 +207,13 @@
     }
   }
 })();
+
+(function(){
+  'use strict';
+  if(document.getElementById('insightDashboardYearFixV1Script'))return;
+  var script=document.createElement('script');
+  script.id='insightDashboardYearFixV1Script';
+  script.src='./insight_dashboard_year_fix_v1.js?v=20260921-1';
+  script.async=false;
+  document.head.appendChild(script);
+})();
